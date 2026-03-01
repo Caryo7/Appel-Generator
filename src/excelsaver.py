59c0a8585470fad782ba141180d21cdc12b92f31
Benoit Charreyron <benoit.charreyron@orange.fr>
2026-03-01 15:04:20 +0100

    Update excelsaver.py

--- src/excelsaver.py
+++ src/excelsaver.py
@@ -5,7 +5,21 @@ excel = DispatchEx('Excel.Application')
 excel.Visible = 0
 
 def export_pdf(path_from, path_to = None):
+    """Enregistrement du fichier excel. Utilise une
+    bibliothèque de control de Excel pour demander à excel de
+    faire le travail.
+
+    Arguments
+    * path_from: le fichier excel à exporter
+    * path_to: le fichier PDF de sortie
+
+    Retourne
+    * path_to
+    """
+
     if path_to is None:
+        # Si on ne donne pas de destination, on prendra le même
+        # nom que le fichier excel
         path_to = path_from.replace('.xlsx', '.pdf')
 
     try:
@@ -16,6 +30,7 @@ def export_pdf(path_from, path_to = None):
         wb.Close()
 
     except Exception as e:
+        # Si erreur d'exportation on retournera faux, pour erreur
         dialogs.warning("Erreur d'exportation ! Fermez les taches Excel déjà démarrées")
         excel.Quit()
         return False
